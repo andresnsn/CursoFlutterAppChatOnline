6f746ecaac7ccacb76efc66ad84c6e1176156f09
andresnsn <59550765+andresnsn@users.noreply.github.com>
2020-07-24 16:37:06 -0300

    AppChatOnline

---
+++ android/app/src/main/kotlin/sinka/com/br/chatonlineapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package sinka.com.br.chatonlineapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:chat/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        iconTheme: IconThemeData(
+          color: Colors.blue)
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
+
